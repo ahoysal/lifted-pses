b1c67766a7df6a9c158ccb6695c4cd6a58ed0604
ahoysal <aagrim@ucsb.edu>
2026-04-08 11:35:01 -0700

    best validation now gets used for final testing

--- training.py
+++ training.py
@@ -4,10 +4,13 @@ from torch_geometric.loader import DataLoader
 from sklearn.metrics import average_precision_score
 from torch_geometric.nn import global_mean_pool
 import numpy as np
+import copy
 
 def train(model: torch.nn.Module, dataset: tg.Dataset | dict[str, tg.Dataset], epochs: int):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model.to(device)
+
+    best = (0, None)
 
     masked = not isinstance(dataset, dict)
 
@@ -15,6 +18,7 @@ def train(model: torch.nn.Module, dataset: tg.Dataset | dict[str, tg.Dataset], e
     trainDataset = dataset if masked else dataset["train"]
     trainLoader = DataLoader(trainDataset, batch_size=32, shuffle=True)
     valLoader = DataLoader(trainDataset if masked else dataset["val"], batch_size=32, shuffle=True)
+    testLoader = DataLoader(trainDataset if masked else dataset["test"], batch_size=32, shuffle=True)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     # lossFunction = torch.nn.CrossEntropyLoss()
@@ -67,13 +71,52 @@ def train(model: torch.nn.Module, dataset: tg.Dataset | dict[str, tg.Dataset], e
                     all_targets.append(data.y.cpu().numpy())
 
         if masked:
-            test_acc = total_correct / total_test_nodes
-            print(f"Epoch {epoch+1:03d} | Loss: {total_loss:.4f} | Test Acc: {test_acc:.4f}")
+            metric = total_correct / total_test_nodes
+            print(f"Epoch {epoch+1:03d} | Loss: {total_loss:.4f} | Val Acc: {metric:.4f}")
         else:
             # Concatenate lists of arrays into single large arrays
             full_preds = np.concatenate(all_preds, axis=0)
             full_targets = np.concatenate(all_targets, axis=0)
             
             # Calculate AP on the entire validation set at once
-            test_ap = average_precision_score(full_targets, full_preds)
-            print(f"Epoch {epoch+1:03d} | Loss: {total_loss:.4f} | Test AP: {test_ap:.4f}")
+            metric = average_precision_score(full_targets, full_preds)
+            print(f"Epoch {epoch+1:03d} | Loss: {total_loss:.4f} | Val AP: {metric:.4f}")
+
+        if metric > best[0]:
+            best = (metric, copy.deepcopy(model.state_dict()))
+            print("[TEST]: better %f!" % metric)
+
+    if best[1] is not None:
+        model.load_state_dict(best[1])
+    
+    total_correct = 0
+    total_test_nodes = 0
+    all_preds = []
+    all_targets = []
+
+    with torch.no_grad():
+        for data in testLoader:
+            data = data.to(device)
+
+            out = model(data)
+            if masked:
+                pred = out.argmax(dim=1)
+                test_correct = (pred[data.test_mask] == data.y[data.test_mask]).sum()
+
+                total_correct += test_correct.item()
+                total_test_nodes += data.test_mask.sum().item()
+            else:
+                all_preds.append(out.detach().cpu().numpy())
+                all_targets.append(data.y.cpu().numpy())
+
+    if masked:
+        metric = total_correct / total_test_nodes
+        print(f"Final Test Acc: {metric:.4f}")
+    else:
+        # Concatenate lists of arrays into single large arrays
+        full_preds = np.concatenate(all_preds, axis=0)
+        full_targets = np.concatenate(all_targets, axis=0)
+        
+        # Calculate AP on the entire validation set at once
+        metric = average_precision_score(full_targets, full_preds)
+        print(f"Final Test AP: {metric:.4f}")
